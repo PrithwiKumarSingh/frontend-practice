0d864eaa4e5f5b032ce7a0c5f1bab3d2245a2021
PrithwiKumarSingh <prithwikumar871@gmail.com>
2026-03-08 23:30:06 +0530

    notfound page

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
       <Route path='/' element={<Home></Home>}></Route>
       <Route path='/projects' element={<Projects/>}></Route>
       {/* <Route path='/notfound' element={<NotFound/>}></Route> */}
-      <Route path='/pri' element={<NotFound/>}></Route>
+      <Route path='*' element={<NotFound/>}></Route>
     </Routes>
 
 
